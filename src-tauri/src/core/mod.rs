f318cd094f591223c404f0cdf77308c8575a1f34
qzs <qzsiniong@163.com>
2025-07-24 22:58:13 +0800

    feat: log colored

--- src-tauri/src/core/mod.rs
+++ src-tauri/src/core/mod.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 use tauri::{App, AppHandle};
-use tauri_plugin_log::{Target, TargetKind};
+use tauri_plugin_log::{fern::colors::ColoredLevelConfig, Target, TargetKind, TimezoneStrategy};
 
 mod device;
 pub mod pin;
@@ -20,12 +20,23 @@ fn setup_log(app: &App) -> Result<(), Box<dyn Error>> {
     if cfg!(debug_assertions) {
         app.handle().plugin(
             tauri_plugin_log::Builder::default()
+                .timezone_strategy(TimezoneStrategy::UseLocal)
                 .targets([
                     Target::new(TargetKind::Stdout),
                     Target::new(TargetKind::LogDir { file_name: None }),
                     Target::new(TargetKind::Webview),
                 ])
-                .level(log::LevelFilter::Info)
+                .level({
+                    #[cfg(debug_assertions)]
+                    {
+                        log::LevelFilter::Debug
+                    }
+                    #[cfg(not(debug_assertions))]
+                    {
+                        log::LevelFilter::Info
+                    }
+                })
+                .with_colors(ColoredLevelConfig::new())
                 .build(),
         )?;
     }
