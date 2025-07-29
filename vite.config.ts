c00b7ba0cb7517525798665c5fa3f2d9b3f9b6a4
qzs <qzsiniong@163.com>
2025-07-29 15:02:54 +0800

    chore: cargo workspace

--- vite.config.ts
+++ vite.config.ts
@@ -78,7 +78,7 @@ export default defineConfig(async () => ({
       : undefined,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
-      ignored: ['**/src-tauri/**'],
+      ignored: ['**/src-tauri/**', 'target'],
     },
   },
 }))
